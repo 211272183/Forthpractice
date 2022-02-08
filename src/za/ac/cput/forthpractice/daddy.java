630ad62001a028bf35685e50cf2300da4a82bcc6
211272183 <211272183@mycput.ac.za>
2022-02-08 11:32:12 +0200

    second commit

--- src/za/ac/cput/forthpractice/daddy.java
+++ src/za/ac/cput/forthpractice/daddy.java
@@ -3,5 +3,6 @@ package za.ac.cput.forthpractice;
 public class daddy {
     public static void main(String[] args) {
         System.out.println("hello high");
+        System.out.println("hope i got it right");
     }
 }
